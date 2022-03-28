cc87b94065a557dd2006d734b9960557c36b8eef
ahlblzk <ahlblzk@gmail.com>
2022-03-28 18:25:00 +0800

    benchmarks基准测试

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeateCount = 5
+
 func Repeat(str string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + str
+	for i := 0; i < repeateCount; i++ {
+		repeated += str
 	}
 	return repeated
 }

1fe9232767017dce86d0a5f06b9caa7229b81c63
ahlblzk <ahlblzk@gmail.com>
2022-04-02 23:13:51 +0800

    range方法使用

--- arrays/sum.go
+++ arrays/sum.go
@@ -2,8 +2,9 @@ package arrays
 
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	//range 会迭代数组，每次迭代都会返回数组元素的索引和值。我们选择使用 _ 空白标志符 来忽略索引
+	for _, v := range numbers {
+		sum += v
 	}
 	return sum
 }
